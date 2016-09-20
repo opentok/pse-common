bc4e66353cf23df8b64f6d44de15e89febadc3ff
Cristian Rodriguez <crdlc@tid.es>
2016-09-20 11:34:40 +0200

    First version

---
+++ web/js/helpers/navigator/navigator.js
@@ -0,0 +1,136 @@
+!function(global) {
+  'use strict';
+
+  var position = {
+    LEFT: 'left',
+    RIGHT: 'right',
+    TOP: 'top',
+    DOWN: 'down',
+    HOME: 'home'
+  };
+
+  var oppositePosition = {
+    left: position.RIGHT,
+    right: position.LEFT,
+    top: position.DOWN,
+    down: position.TOP
+  };
+
+  /**
+   * The view disappears in favour of the new view
+   *
+   * @param {DOMElement} the view
+   *
+   * @param {String} value
+   *
+   * @param {String} view will be translated from this position
+   *
+   */
+  function toggle(view, pos) {
+    view.dataset.position = oppositePosition[pos];
+  }
+
+  var Handler = function(container) {
+    var currentCard;
+    var cards = {};
+    var history = [];
+    var childNodes = container.childNodes;
+    for (var count = 0, length = childNodes.length; count < length; count++) {
+      var child = childNodes[count];
+      if (child.classList && child.classList.contains('card')) {
+        cards[child.id] = child;
+        if (child.dataset.position === position.HOME) {
+          currentCard = child;
+          history.push(currentCard);
+          document.body.dataset.currentCard = child.id;
+        }
+      }
+    }
+
+    return {
+      /**
+       * It goes to first card
+       *
+       */
+      reset: function() {
+        var isNotTheLastCard = this.back();
+        while (isNotTheLastCard) {
+          isNotTheLastCard = this.back();
+        };
+      },
+
+      /**
+       * Returns the view by identifier
+       *
+       * @param {String} identifier of the view
+       *
+       */
+      get: function(id) {
+        return cards[id];
+      },
+
+      /**
+       * Navigates to a view by identifier
+       *
+       * @param {String} identifier of the view
+       *
+       */
+      go: function(id) {
+        if (id === currentCard.id) {
+          return;
+        }
+
+        var goCard = cards[id];
+        if (goCard) {
+          toggle(currentCard, goCard.dataset.position);
+          currentCard = goCard;
+          history.push(currentCard);
+          document.body.dataset.currentCard = id;
+          currentCard.dataset.position = position.HOME;
+        }
+      },
+
+      /**
+       * Navigates to the previous view
+       */
+      back: function() {
+        var count = history.length;
+        if (count <= 1) {
+          return false;
+        }
+
+        currentCard = history[count - 2];
+        var lastCard = history.pop();
+        toggle(lastCard, currentCard.dataset.position);
+        currentCard.dataset.position =  position.HOME;
+        return true;
+      },
+
+      /**
+       * Returns the current view
+       */
+      get current() {
+        return currentCard;
+      },
+
+      destroy: function() {
+        currentCard = null;
+        cards = null;
+        history = null;
+        childNodes = null;
+      }
+    };
+  };
+
+  var _instances = [];
+  global.Navigator = {
+    get: function(sel) {
+      var element = typeof sel === 'string' ? document.querySelector(sel) : sel;
+      var instance = _instances.find(inst => inst.element === element);
+      !instance && (instance =
+        _instances[_instances.push({ element: element, handler: new Handler(element)}) - 1]);
+      return instance.handler;
+    }
+  };
+
+}(this);
---
+++ web/js/helpers/requests.js
@@ -0,0 +1,62 @@
+!function(exports) {
+  'use strict';
+
+  var server = window.location.origin;
+
+  var debug =
+    new Utils.MultiLevelLogger('requests.js', Utils.MultiLevelLogger.DEFAULT_LEVELS.all);
+
+  function sendXHR(aType, aURL, aData, aDataType, aResponseType) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(aType, aURL);
+      xhr.responseType = aResponseType || 'json';
+      xhr.overrideMimeType && xhr.overrideMimeType('application/json');
+      if (aDataType) {
+        // Note that this requires
+        xhr.setRequestHeader('Content-Type', aDataType);
+      }
+
+      xhr.onload = function (aEvt) {
+        if (xhr.status === 200) {
+          var response = xhr.responseType === 'json' && xhr.response || xhr.responseText;
+          if (xhr.responseType === 'json' && typeof xhr.response === 'string') {
+            response = JSON.parse(response);
+          }
+          resolve(response);
+        } else {
+          reject({ status: xhr.status, reason: xhr.response });
+        }
+      };
+
+      xhr.onerror = function (aEvt) {
+        debug.error('sendXHR. XHR failed ' + JSON.stringify(aEvt) + 'url: '+
+                    aURL + ' Data: ' + aData + ' RC: ' + xhr.responseCode);
+        reject(aEvt);
+      };
+
+      xhr.send(aData);
+    });
+  }
+
+  function composeDate(data) {
+    var composed = [];
+
+    Object.keys(data).forEach(function(key) {
+      composed.push(key);
+      composed.push('=');
+      composed.push(data[key]);
+      composed.push('&');
+    });
+
+    composed.length && composed.pop();
+
+    return composed.join('');
+  }
+
+  var Request = {
+    sendXHR: sendXHR
+  };
+
+  exports.Request = Request;
+}(this);
